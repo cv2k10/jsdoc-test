6230ed6f4ed5c186be256534638b795f07ab0420
calvin <cv2k10@gmail.com>
2021-07-28 23:10:32 +0800

    add Test component

---
+++ src/Test.js
@@ -0,0 +1,19 @@
+/**
+ * Component for Test.
+ *
+ * @component
+ * @example
+ * return (
+ *   <Test />
+ * )
+ * 
+ * 
+ * @category Testing
+ */
+function Test() {
+    return (
+        <p>Testing</p>
+    )
+}
+
+export default Test;
